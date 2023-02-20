e83489aa60a1ebd1ffe91eaa7d9513dedd273ce6
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-02-20 16:16:32 -0500

    added product and category models to models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // ID
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Product Name
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // Price
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // Stock
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    }
   },
   {
     sequelize,
